45904a862e6d9864dfec8a869ccc4d47b1e70ba2
John <e.k@mail.ru>
2020-10-22 23:29:21 +0300

    Update run.py

--- visiobas_gateway/run.py
+++ visiobas_gateway/run.py
@@ -16,8 +16,10 @@ def main():
 
     # Setting the VisioGateway logging level
     # todo: change to logging level by param
+    # level being 'DEBUG, INFO, WARNING, ERROR'
     # level = logging.DEBUG if args.debug else logging.INFO
-    level = logging.INFO
+
+    level = logging.ERROR
     logging.basicConfig(format=LOGGER_FORMAT,
                         level=level,
                         stream=sys.stdout)
