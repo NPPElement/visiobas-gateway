78b69388c4612b1f6d4b33c9f2bab15ac224e983
John <e.k@mail.ru>
2020-10-31 11:29:11 +0300

    Структура данных

--- visiobas_gateway/run.py
+++ visiobas_gateway/run.py
@@ -2,21 +2,16 @@ import argparse
 import logging
 import sys
 
-from gateway.gateway.visio_gateway import VisioGateway
+from visiobas_gateway.gateway.visio_gateway import VisioGateway
 
 LOGGER_FORMAT = '%(levelname)-8s [%(asctime)s] [%(threadName)s] %(name)s - (%(filename)s).%(funcName)s(%(lineno)d): %(message)s'
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Polling devices and sends data to server.')
-
-    parser.add_argument('-d', '--debug', action='store_true', help='Enable detailed logs.')
-    # args = parser.parse_args()
-
     # Setting the VisioGateway logging level
     # todo: change to logging level by param
     # level being 'DEBUG, INFO, WARNING, ERROR'
+    
     # level = logging.DEBUG 
     # level = logging.INFO
     # level = logging.WARNING
