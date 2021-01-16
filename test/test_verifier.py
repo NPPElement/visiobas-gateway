103391fc324419663f2d80b8388cb16a17805eaa
mtovts <lllghtz@outlook.com>
2021-01-16 16:03:00 +0300

    tests (unfinished)

---
+++ test/test_verifier.py
@@ -0,0 +1,202 @@
+import unittest
+
+from bacpypes.basetypes import PriorityArray, PriorityValue
+from bacpypes.primitivedata import Null
+
+from vb_gateway.connectors.bacnet import ObjType, ObjProperty, StatusFlags
+from vb_gateway.gateway.verifier import BACnetVerifier
+
+
+class TestVerify(unittest.TestCase):
+    def setUp(self) -> None:
+        config = {
+            "http_enable": False,
+            "mqtt_enable": False
+        }
+        self.verifier = BACnetVerifier(protocols_queue=None,
+                                       http_queue=None,
+                                       config=config
+                                       )
+
+    def tearDown(self) -> None:
+        del self.verifier
+
+    def test_verify_unfilled(self):
+        cases = (
+            {},
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT},
+            {ObjProperty.objectIdentifier: 13},
+            {ObjProperty.presentValue: 666.0},
+            {ObjProperty.objectIdentifier: 13, ObjProperty.presentValue: 666.0},
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT, ObjProperty.presentValue: 666.0}
+        )
+        for case in cases:
+            with self.subTest(case=case):
+                self.assertRaises(KeyError, self.verifier.verify, case)
+
+    def test_verify_simple_correct(self):
+        cases = (
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 1,
+             ObjProperty.presentValue: 1
+             },
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 2,
+             ObjProperty.presentValue: 2,
+             ObjProperty.reliability: 2
+             },
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 3,
+             ObjProperty.presentValue: 3,
+             ObjProperty.reliability: 3,
+             ObjProperty.statusFlags: StatusFlags()
+             },
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 4,
+             ObjProperty.presentValue: 4,
+             ObjProperty.reliability: 4,
+             ObjProperty.statusFlags: StatusFlags([0, 1, 0, 0])
+             }
+        )
+        expected = (
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 1,
+             ObjProperty.statusFlags: StatusFlags(),
+             ObjProperty.reliability: 0,
+             ObjProperty.presentValue: 1,
+             },
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 2,
+             ObjProperty.presentValue: 2,
+             ObjProperty.statusFlags: StatusFlags(),
+             ObjProperty.reliability: 2
+             },
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 3,
+             ObjProperty.presentValue: 3,
+             ObjProperty.reliability: 3,
+             ObjProperty.statusFlags: StatusFlags()
+             },
+            {ObjProperty.objectType: ObjType.ANALOG_INPUT,
+             ObjProperty.objectIdentifier: 4,
+             ObjProperty.presentValue: 4,
+             ObjProperty.reliability: 4,
+             ObjProperty.statusFlags: StatusFlags([0, 1, 0, 0])
+             }
+        )
+        for case, _expected in zip(cases, expected):
+            with self.subTest(case=case):
+                _result = self.verifier.verify(obj_properties=case)
+                self.assertDictEqual(_expected, _result)
+
+    def test_verify_binary(self):
+        cases = (
+            {ObjProperty.objectType: ObjType.BINARY_INPUT,
+             ObjProperty.objectIdentifier: 1,
+             ObjProperty.presentValue: 'active'
+             },
+            {ObjProperty.objectType: ObjType.BINARY_INPUT,
+             ObjProperty.objectIdentifier: 2,
+             ObjProperty.presentValue: 'inactive',
+             ObjProperty.reliability: 2,
+             ObjProperty.statusFlags: StatusFlags()
+             }
+        )
+        expected = (
+            {ObjProperty.objectType: ObjType.BINARY_INPUT,
+             ObjProperty.objectIdentifier: 1,
+             ObjProperty.statusFlags: StatusFlags(),
+             ObjProperty.reliability: 0,
+             ObjProperty.presentValue: 1,
+             },
+            {ObjProperty.objectType: ObjType.BINARY_INPUT,
+             ObjProperty.objectIdentifier: 2,
+             ObjProperty.presentValue: 0,
+             ObjProperty.statusFlags: StatusFlags(),
+             ObjProperty.reliability: 2
+             }
+        )
+        for case, _expected in zip(cases, expected):
+            with self.subTest(case=case):
+                _result = self.verifier.verify(obj_properties=case)
+                self.assertDictEqual(_expected, _result)
+
+    def test_verify_with_priority_array(self):
+        cases = (
+            {ObjProperty.objectType: ObjType.ANALOG_VALUE,
+             ObjProperty.objectIdentifier: 1,
+             ObjProperty.presentValue: 1,
+             ObjProperty.priorityArray: PriorityArray(
+                 [PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null())])
+             },
+            {ObjProperty.objectType: ObjType.MULTI_STATE_OUTPUT,
+             ObjProperty.objectIdentifier: 2,
+             ObjProperty.presentValue: 2.,
+             ObjProperty.reliability: 2,
+             ObjProperty.priorityArray: PriorityArray(
+                 [PriorityValue(null=Null()), PriorityValue(real=2.2),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null())])
+             },
+            {ObjProperty.objectType: ObjType.BINARY_OUTPUT,
+             ObjProperty.objectIdentifier: 3,
+             ObjProperty.presentValue: 3.,
+             ObjProperty.priorityArray: PriorityArray(
+                 [PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(real=3.3),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null()),
+                  PriorityValue(null=Null()), PriorityValue(null=Null())])
+             }
+        )
+
+        expected = (
+            {ObjProperty.objectType: ObjType.ANALOG_VALUE,
+             ObjProperty.objectIdentifier: 1,
+             ObjProperty.statusFlags: StatusFlags(),
+             ObjProperty.reliability: 0,
+             ObjProperty.presentValue: 1,
+             ObjProperty.priorityArray: ('', '', '', '', '', '', '', '',
+                                         '', '', '', '', '', '', '', '')
+             },
+            {ObjProperty.objectType: ObjType.MULTI_STATE_OUTPUT,
+             ObjProperty.objectIdentifier: 2,
+             ObjProperty.presentValue: 2.,
+             ObjProperty.statusFlags: StatusFlags(),
+             ObjProperty.reliability: 2,
+             ObjProperty.priorityArray: ('', 2, '', '', '', '', '', '',
+                                         '', '', '', '', '', '', '', '')
+             },
+            {ObjProperty.objectType: ObjType.BINARY_OUTPUT,
+             ObjProperty.objectIdentifier: 3,
+             ObjProperty.presentValue: 3.,
+             ObjProperty.statusFlags: StatusFlags([0, 0, 1, 0]),
+             ObjProperty.reliability: 0,
+             ObjProperty.priorityArray: ('', '', '', '', '', '', '', 3,
+                                         '', '', '', '', '', '', '', '')
+             }
+        )
+        for case, _expected in zip(cases, expected):
+            with self.subTest(case=case):
+                _result = self.verifier.verify(obj_properties=case)
+                print(f'Res: {_result}', f"Exp: {_expected}", sep='\n')
+                self.assertDictEqual(_expected, _result)
+
+
+if __name__ == '__main__':
+    unittest.main()
