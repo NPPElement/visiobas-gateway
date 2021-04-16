f9035dda910d4436be3b722d5e9285c7c9644da5
mtovts <lllghtz@outlook.com>
2021-04-17 02:55:24 +0300

    Fix: ModbusPropertyListWrapper,  imports

--- gateway/models/bacnet/device_obj.py
+++ gateway/models/bacnet/device_obj.py
@@ -1,23 +1,38 @@
-from pydantic import Field, validator
+from typing import Union
+
+from pydantic import Field, validator, BaseModel
 
 from .base_obj import BaseBACnetObjModel
 from .obj_property import ObjProperty
-from ..modbus.device_rtu_property_list import (DeviceRTUPropertyListWrapper,
-                                               DeviceRTUPropertyListModel)
+
+
+# from ..modbus.device_rtu_property_list import DeviceRTUPropertyListModel
+
+
+class DeviceRTUPropertyListModel(BaseModel):
+    port: str = Field(...)
+    baudrate: int = Field(..., gt=0, lt=115200)
+    stopbits: int = Field(default=1)
+    bytesize: int = Field(default=8)
+    timeout: float = Field(default=1)
+    parity: str = Field(default='N')
+
+
+class DevicePropertyListWrapper(BaseModel):
+    rtu: DeviceRTUPropertyListModel
 
 
 class BACnetDeviceModel(BaseBACnetObjModel):
     timeout: int = Field(..., alias=ObjProperty.apduTimeout.id_str)
     retries: int = Field(default=1, alias=ObjProperty.numberOfApduRetries.id_str)
 
-    # todo use Ip obj
-    address: str = Field(..., alias=ObjProperty.deviceAddressBinding.id_str)
-
-    # todo use Protocol Enum
-    protocol: str = Field(..., alias=ObjProperty.protocolVersion.id_str)
+    address: str = Field(...,
+                         alias=ObjProperty.deviceAddressBinding.id_str)  # todo use Ip obj
+    protocol: str = Field(...,
+                          alias=ObjProperty.protocolVersion.id_str)  # todo use Protocol Enum
 
     # todo refactor
-    property_list: DeviceRTUPropertyListWrapper = Field(
+    property_list: Union[str, DevicePropertyListWrapper] = Field(
         alias=ObjProperty.propertyList.id_str)
 
     @validator('property_list')
--- gateway/models/modbus/__init__.py
+++ gateway/models/modbus/__init__.py
@@ -1,9 +1,9 @@
-from .device_rtu import ModbusRTUDeviceModel
+# from .device_rtu import ModbusRTUDeviceModel
 from .func_code import ModbusFunc, MODBUS_READ_FUNCTIONS, MODBUS_WRITE_FUNCTIONS
 from .obj import ModbusObjModel
 
 __all__ = [
     'ModbusFunc', 'MODBUS_READ_FUNCTIONS', 'MODBUS_WRITE_FUNCTIONS',
     'ModbusObjModel',
-    'ModbusRTUDeviceModel',
+    # 'ModbusRTUDeviceModel',
 ]
