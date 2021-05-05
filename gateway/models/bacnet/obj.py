acdca965eb697ce585beb4be87292c6073a4db1c
mtovts <lllghtz@outlook.com>
2021-05-05 13:11:27 +0300

    Add to_str methods to obj

--- gateway/models/bacnet/obj.py
+++ gateway/models/bacnet/obj.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, Union
 
-from pydantic import Field, validator, BaseModel
+from pydantic import Field, BaseModel
 
 from .base_obj import BaseBACnetObjModel
 from .obj_property import ObjProperty
@@ -22,10 +22,13 @@ class BACnetObjPropertyListJsonModel(BaseModel):
 
 
 class BACnetObjModel(BaseBACnetObjModel):
-    # present_value: float = Field(alias=ObjProperty.presentValue.id_str)
-    # status_flags: Union[list[bool], None] = Field(alias=ObjProperty.statusFlags.id_str)
-    # priority_array: Union[str, None] = Field(alias=ObjProperty.priorityArray.id_str)
-    # reliability: Union[str, None] = Field(alias=ObjProperty.reliability.id_str)
+    pv: float = Field(alias=ObjProperty.presentValue.id_str, description='Present value')
+    sf: Union[int, list[bool]] = Field(default=0b0000, alias=ObjProperty.statusFlags.id_str,
+                                       description='Status flags')
+    pa: Union[str, tuple, None] = Field(alias=ObjProperty.priorityArray.id_str,
+                                        description='Priority array')
+    reliability: Union[int, str, None] = Field(default=0,
+                                               alias=ObjProperty.reliability.id_str)
 
     resolution: Optional[float] = Field(default=0.1, alias=ObjProperty.resolution.id_str)
 
@@ -42,6 +45,40 @@ class BACnetObjModel(BaseBACnetObjModel):
 
     def __repr__(self) -> str:
         return f'BACnetObj{self.__dict__}'
+
+    def to_mqtt_str(self) -> str:
+        return '{0} {1} {2} {3} {4}'.format(self.device_id,
+                                            self.id,
+                                            self.type.id,
+                                            self.pv,
+                                            self.sf, )
+
+    def to_http_str(self) -> str:
+        str_ = '{0} {1} {2}'.format(self.id,
+                                    self.type.id,
+                                    self.pv, )
+        if self.pa is not None:
+            pa_str = self._convert_pa_to_str(pa=self.pa)
+            str_ += ' ' + pa_str
+
+        str_ += ' ' + str(self.sf)
+
+        if self.reliability:
+            str_ += ' ' + str(self.reliability)
+
+        return str_
+
+
+    @staticmethod
+    def _convert_pa_to_str(pa: tuple) -> str:
+        """Convert priority array tuple to str.
+
+        Result example: ,,,,,,,,40.5,,,,,,49.2,
+        """
+        return ','.join(
+            ['' if priority is None else str(priority)
+             for priority in pa]
+        )
 
     # @validator('poll_interval')
     # def set_default_poll_interval(cls, v):
