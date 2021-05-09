7b8f00bba34e2435cd9efd8be540d2b331961ea2
mtovts <lllghtz@outlook.com>
2021-05-09 22:05:15 +0300

    Add VisioGatewayConfig model

---
+++ gateway/models/config/gateway.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel, Field, HttpUrl, validator
+from yarl import URL
+
+from .http import VisioHTTPConfig
+
+
+class VisioGatewayConfig(BaseModel):
+    update_period: int = Field(default=3600, ge=1800)
+    verifier_override_threshold: int = Field(default=8, gt=0, le=16,
+                                             description=(
+                                                 'if priority is equal or greater than '
+                                                 'this value - verifier sets '
+                                                 'the OVERRIDEN flag'))
+    api_url: HttpUrl = Field(default='http://localhost:7070')
+
+    # Paths
+    base_dir = Path(__file__).resolve().parent.parent.parent.parent
+    default_cfg_dir = Path(os.getenv('cfg_dir', str(base_dir / 'config')))
+    cfg_dir: Path = Field(default=default_cfg_dir)
+
+    http_cfg_path = cfg_dir / 'http.json'
+    mqtt_cfg_path = cfg_dir / 'mqtt.json'
+
+    http: Optional[VisioHTTPConfig] = Field(
+        default=VisioHTTPConfig.parse_raw(http_cfg_path.read_text()))
+    mqtt: Optional = Field(default=None)  # todo
+
+    @validator('api_url')
+    def cast_url(cls, v: HttpUrl) -> URL:
+        return URL(v)
+
