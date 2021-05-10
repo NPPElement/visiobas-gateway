29a40921063d15b4ac3e480dd2f66079cf422b4b
mtovts <lllghtz@outlook.com>
2021-05-11 00:43:37 +0300

    Update settings models

--- gateway/models/config/gateway_settings.py
+++ gateway/models/config/gateway_settings.py
@@ -16,14 +16,14 @@ class GatewaySettings(BaseSettings):
     api_url: AnyHttpUrl = Field(default='http://localhost:7070')
 
     # Paths
-    config_path: str = Field(default='config',
-                             description='Path to the config directory from the base path.')
+    # config_path: str = Field(default='config',
+    #                          description='Path to the config directory from the base path.')
 
     # http_config_path: FilePath = Field(default=config_dir / 'http.json')
     # mqtt_config_path: FilePath = Field(default=config_dir / 'mqtt.json')
 
     # http: HTTPSettings = Field(...)
-    http: HTTPSettings = HTTPSettings(_env_file='....config/http.env')
+    # http: HTTPSettings = HTTPSettings(_env_file='...config/http.env')
     # str = Field(default='http.env')
     # mqtt_env_file: str = Field(default='mqtt.env')
 
@@ -34,15 +34,15 @@ class GatewaySettings(BaseSettings):
         env_file = '.env'
         env_file_encoding = 'utf-8'
 
-    @property
-    def base_dir(self) -> DirectoryPath:
-        return Path(__file__).resolve().parent.parent.parent.parent
+    # @property
+    # def base_dir(self) -> DirectoryPath:
+    #     return Path(__file__).resolve().parent.parent.parent.parent
+    #
+    # @property
+    # def config_dir(self) -> DirectoryPath:
+    #     return self.base_dir / self.config_path
 
-    @property
-    def config_dir(self) -> DirectoryPath:
-        return self.base_dir / self.config_path
-
-    @property
-    def http(self) -> HTTPSettings:
-        env_path = self.config_dir / 'http.env'
-        return HTTPSettings(_env_file=env_path)
+    # @property
+    # def http(self) -> HTTPSettings:
+    #     env_path = self.config_dir / 'http.env'
+    #     return HTTPSettings(_env_file=env_path)
