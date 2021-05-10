2135dd411d8b2dc5a3b82b76ad387829dc323972
mtovts <lllghtz@outlook.com>
2021-05-11 00:20:50 +0300

    Refactor HTTPServerConfig(BaseModel) + deprecate HTTPNode

--- gateway/models/config/http_server.py
+++ gateway/models/config/http_server.py
@@ -1,8 +1,7 @@
 from hashlib import md5
 from typing import Optional
 
-from pydantic import BaseModel, Field, validator, HttpUrl, SecretStr
-from yarl import URL
+from pydantic import Field, BaseModel, HttpUrl, validator, AnyHttpUrl, BaseSettings
 
 
 class AuthData(BaseModel):
@@ -12,96 +11,53 @@ class AuthData(BaseModel):
 
 
 class HTTPServerConfig(BaseModel):
-    """Represent parameters for Visio HTTP server.
-
-    Singleton.
-
-    # todo: add validator for URL?
-    """
-    login: SecretStr = Field(...)
-    password: SecretStr = Field(...)
-    url: HttpUrl = Field(...)  # in validation: HttpUlr -> URL
-
+    urls: list[AnyHttpUrl] = Field(...)
+    current = 0
     auth_data: Optional[AuthData] = None
-    is_authorized = False
 
-    @validator('url')
-    def cast_url(cls, v: HttpUrl) -> URL:
-        return URL(v)
+    @property
+    def urls_len(self) -> int:
+        return len(self.urls)
 
-    @validator('password')
-    def hash_password(cls, v: str) -> str:
-        return md5(v.encode()).hexdigest()
+    @property
+    def current_server(self) -> AnyHttpUrl:
+        return self.urls[self.current]
 
-    def __hash__(self) -> int:
-        return hash((self.url, self.login))
+    @property
+    def is_authorized(self) -> bool:
+        return bool(self.auth_data)
+
+    @property
+    def auth_payload(self) -> dict[str, str]:
+        return {'login': self.current_server.user,
+                'password': self.current_server.password}
+
+    @property
+    def auth_headers(self) -> Optional[dict[str, str]]:
+        if self.auth_data:
+            return {'Authorization': f'Bearer {self.auth_data.bearer_token}'}
 
     def __str__(self) -> str:
-        _auth_status = 'Authorized' if self.is_authorized else 'Unauthorized'
-        return f'[{_auth_status}:{self.url.host}]'
+        return str(self.current_server.host)
 
     def __repr__(self) -> str:
         return str(self)
 
-    # @property
-    # def is_authorized(self) -> bool:
-    #     return self.is_authorized
-
-    @property
-    def auth_payload(self) -> dict[str, str]:
-        return {'login': self.login, 'password': self.password}
-
-    @property
-    def auth_headers(self) -> dict[str, str]:
-        if self.auth_data:
-            return {'Authorization': f'Bearer {self.auth_data.bearer_token}'}
-
-            # # That class allows to create only one instance for each server's params
-            # _instances = {}  # keeps instances references
-
-            # __slots__ = ('_login', '_password_md5', 'host', 'port',
-            #              '_bearer_token', '_user_id', '_auth_user_id',
-            #              'is_connected', 'is_authorized'
-            #              )
-
-            # def __new__(cls, *args, **kwargs):
-            #     # Used kwargs because __init__ accept args by key
-            #     _args_hash = hash((kwargs.get('login'),
-            #                        kwargs.get('password'),
-            #                        kwargs.get('host'),
-            #                        kwargs.get('port')
-            #                        ))
-            #     if cls._instances.get(_args_hash) is None:
-            #         cls._instances[_args_hash] = super().__new__(cls)
-            #     return cls._instances[_args_hash]
-
-            # def __init__(self, login: str, password: str, host: str, port: int):
-            #     self.host = host
-            #     self.port = port
-            #
-            #     self._login = login
-            #     self._password_md5 = md5(password.encode()).hexdigest()
-            #
-            #     self._bearer_token = None
-            #     self._user_id = None
-            #     self._auth_user_id = None
-            #
-            #     # self.is_connected = True
-            #     self.is_authorized = False
-
     def set_auth_data(self, **kwargs) -> None:
         self.auth_data = AuthData(**kwargs)
-        self.is_authorized = True
 
     def clear_auth_data(self) -> None:
         self.auth_data = None
-        self.is_authorized = False
 
-    # @classmethod
-    # def from_dict(cls, cfg: dict):
-    #     """Create HTTP config for server from dict."""
-    #     return cls(login=cfg['login'],
-    #                password=cfg['password'],
-    #                host=cfg['host'],
-    #                port=cfg.get('port', 8080)
-    #                )
+    def switch_server(self) -> None:
+        """Switches communication from current to next, if it exist.
+
+        Uses in change server when current unavailable.
+        """
+        self.current = self.current + 1 if self.current < self.urls_len - 1 else 0
+
+    @validator('urls')
+    def hash_passwords(cls, v: list[HttpUrl]) -> list[HttpUrl]:
+        for url in v:
+            url.password = md5(url.password.encode()).hexdigest()
+        return v
