8aa0cba5f47fffe673c9f99e047ff92919d4dc6d
mtovts <lllghtz@outlook.com>
2021-05-11 00:18:03 +0300

    update tests for settings

---
+++ tests/models/config/test_http_server.py
@@ -0,0 +1,18 @@
+import unittest
+
+from models.config.http_server import HTTPServerConfig
+
+
+class MyTestCase(unittest.TestCase):
+    def test_parse_raw(self):
+        HTTPServerConfig.parse_raw(self.raw)
+
+    def setUp(self) -> None:
+        self.raw = '''
+        {"urls":["http://login:password@get-main.com:8080",
+                "http://login:password@get-mirror.com:8080"]}
+        '''
+
+
+if __name__ == '__main__':
+    unittest.main()
