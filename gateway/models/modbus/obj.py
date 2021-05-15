3dc063868a602a3629a2ba885098f6685a5ae974
mtovts <lllghtz@outlook.com>
2021-05-15 19:11:29 +0300

    Add validators for casting orders and data_type fields

--- gateway/models/modbus/obj.py
+++ gateway/models/modbus/obj.py
@@ -1,5 +1,9 @@
-from pydantic import Field, Json, BaseModel
+from typing import Union
 
+from pydantic import Field, Json, BaseModel, validator
+from pymodbus.constants import Endian
+
+from .data_type import DataType
 from .func_code import ModbusFunc
 from ..bacnet.obj import BACnetObjModel, BACnetObjPropertyListModel
 from ..bacnet.obj_property import ObjProperty
@@ -16,12 +20,13 @@ class ModbusObjPropertyListModel(BaseModel):
     scale: float = Field(default=1., description='Multiplier `A` for recalculate A*X+B')
     offset: float = Field(default=.0, description='Adding `B` for recalculate A*X+B')
 
-    data_type: str = Field(..., alias='dataType')  # todo Enum
-    data_length: int = Field(default=16, gt=1, lt=64, alias='dataLength',  # todo calc default: quantity *16
+    data_type: Union[DataType, str] = Field(..., alias='dataType')  # todo Enum
+    data_length: int = Field(default=16, gt=1, lt=64, alias='dataLength',
+                             # todo calc default: quantity *16
                              description='The number of bits in which the value is stored')
 
-    byte_order: str = Field(default='little', alias='byteOrder')
-    word_order: str = Field(default='big', alias='wordOrder')
+    byte_order: Union[str, Endian] = Field(default='little', alias='byteOrder')
+    word_order: Union[str, Endian] = Field(default='big', alias='wordOrder')
     repack: bool = Field(default=False)  # todo add to docs
 
     # bitmask = int todo
@@ -29,6 +34,20 @@ class ModbusObjPropertyListModel(BaseModel):
 
     def __repr__(self) -> str:
         return str(self.__dict__)
+
+    @validator('byte_order')
+    def cast_byte_order(cls, v: str) -> Endian:
+        return Endian.Big if v == 'big' else Endian.Little
+
+    @validator('word_order')
+    def cast_word_order(cls, v: str) -> Endian:
+        return Endian.Big if v == 'big' else Endian.Little
+
+    @validator('data_type')
+    def cast_data_type(cls, v: Union[DataType, str]) -> DataType:
+        if isinstance(v, str):
+            return DataType(v.lower())
+        return v
 
 
 class ModbusPropertyListJsonModel(BACnetObjPropertyListModel):
