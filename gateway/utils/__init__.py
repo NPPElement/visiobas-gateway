73bed05120148a21e8c1dfa664f632d2098e830a
mtovts <lllghtz@outlook.com>
2021-05-18 21:09:10 +0300

    Add Singleton metaclass to utils

--- gateway/utils/__init__.py
+++ gateway/utils/__init__.py
@@ -1,4 +1,5 @@
 from .log import get_file_logger, disable_loggers
+from .singleton import Singleton
 from .utils import (read_address_cache,
                     cast_to_bit, cast_2_registers,
                     get_fault_obj_properties,
@@ -10,4 +11,6 @@ __all__ = ['read_address_cache',
            'cast_2_registers',
            'get_file_logger',
            'disable_loggers',
+
+           # 'Singleton',
            ]
