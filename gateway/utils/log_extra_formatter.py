679a737d98153a11d6ed4ea1259c35d039bd1af3
mtovts <lllghtz@outlook.com>
2021-05-21 01:48:15 +0300

    add extra formatter

---
+++ gateway/utils/log_extra_formatter.py
@@ -0,0 +1,11 @@
+import logging
+
+
+class ExtraFormatter(logging.Formatter):
+
+    def format(self, record):
+        string = super().format(record)
+        extra = {k: v for k, v in record.__dict__.items()}
+        if len(extra) > 0:
+            string += " - extra: " + str(extra)
+        return string
