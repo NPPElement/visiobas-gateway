0e93306a257800d22de8dad163a8a5dbf0479a39
mtovts <lllghtz@outlook.com>
2021-05-30 22:38:59 +0300

    fix _round()

--- gateway/models/bacnet/obj.py
+++ gateway/models/bacnet/obj.py
@@ -55,8 +55,6 @@ class BACnetObjPropertyListJsonModel(BaseModel):
 
 
 class BACnetObj(BaseBACnetObjModel):
-    # DEFAULT_RESOLUTION = 0.1
-
     resolution: Optional[Union[int, float]] = Field(
         default=0.1, alias=ObjProperty.resolution.id_str, gt=0,
         description='''
@@ -162,13 +160,14 @@ class BACnetObj(BaseBACnetObjModel):
                    resolution: Union[int, float]) -> float:
             assert isinstance(resolution, (int, float))
 
+            rounded = round(value_ / resolution) * resolution
+
             if isinstance(resolution, int):
-                # FIXME
-                return round(value_)
+                return rounded
             elif isinstance(resolution, float):
                 whole_part, fractional_part = str(resolution).split('.', maxsplit=1)
                 digits = len(fractional_part)
-                return round(round(value_ / resolution) * resolution, ndigits=digits)
+                return round(rounded, ndigits=digits)
 
         self._updated_at = datetime.now()
 
