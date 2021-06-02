5e7806da23ee7c53071f65e45097a46ad076c324
mtovts <lllghtz@outlook.com>
2021-06-02 21:58:50 +0300

    Refactor ModbusRW mixin

--- gateway/api/mixins/modbus_rw.py
+++ gateway/api/mixins/modbus_rw.py
@@ -1,53 +1,50 @@
-from logging import getLogger
-from typing import Any
+from typing import Optional, Union
 
-from ...connectors import ModbusDevice
-from ...models import ModbusObj, ObjProperty
+from ...devices import AsyncModbusDevice  # todo use alias
+from ...utils import get_file_logger
 
-_log = getLogger(__name__)
+_LOG = get_file_logger(name=__name__)
+
+# Aliases
+AsyncModbusDeviceAlias = '...devices.AsyncModbusDevice'
+ModbusObjAlias = '...models.ModbusObj'
 
 
 class ModbusRWMixin:
-
     @staticmethod
-    def read_modbus(obj: ModbusObj, device: ModbusDevice,
-                    prop: ObjProperty = ObjProperty.presentValue) -> Any:
-        try:
-            registers = device.read(obj=obj)
+    def read_modbus(obj: ModbusObjAlias, device: AsyncModbusDevice,
+                    # prop: ObjProperty = ObjProperty.presentValue
+                    ) -> Optional[Union[int, float]]:
+        """
+        Args:
+            obj: Object instance.
+            device: Device instance.
 
-            if obj.properties.quantity == 1:
-                return registers.pop()
-            else:
-                raise NotImplementedError
+        Returns:
+            Read value.
+        """
+        try:
+            value = device.read(obj=obj)  # FIXME async
+            return value
 
         except Exception as e:
-            _log.error(f'Error: {e}',
-                       exc_info=True
-                       )
+            _LOG.exception('Unhandled error',
+                           extra={'device_id': device.id, 'object_id': obj.id, 'exc': e, })
 
     @staticmethod
-    def write_modbus(value, prop: ObjProperty, priority: int,
-                     obj: ModbusObj, device: ModbusDevice) -> None:
+    def write_modbus(value: Union[int, float],
+                     obj: ModbusObjAlias, device: AsyncModbusDevice,
+                     # prop: ObjProperty = ObjProperty.presentValue,
+                     priority: int = 11, ) -> None:
+        """
+        Args:
+            value: Value to write.
+            obj: Object instance.
+            device: Device instance.
+            priority: Priority of value.
+        """
         try:
-            device.write(values=value,
-                         obj=obj
-                         )
+            device.write(value=value, obj=obj)  # FIXME async
         except Exception as e:
-            _log.error(f'Error: {e}',
-                       exc_info=True
-                       )
-
-    def write_with_check_modbus(self, value, prop: ObjProperty, priority: int
-                                , obj: ModbusObj, device: ModbusDevice
-                                ) -> bool:  # tuple[bool, tuple[Any, Any]]:
-        """
-        :return: the read value is equal to the written value
-        """
-        self.write_modbus(value=value,
-                          obj=obj,
-                          device=device
-                          )
-        rvalue = self.read_modbus(obj=obj,
-                                  device=device
-                                  )
-        return value == rvalue  # , (value, rvalue))
+            _LOG.exception('Unhandled error',
+                           extra={'device_id': device.id, 'object_id': obj.id, 'exc': e, })
