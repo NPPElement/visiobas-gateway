01844d4f62fabcda707ca8d713111a5ba9cfe089
mtovts <lllghtz@outlook.com>
2021-06-11 20:43:51 +0300

    logs

--- gateway/gateway_.py
+++ gateway/gateway_.py
@@ -256,10 +256,10 @@ class VisioBASGateway:
 
             self._devices.update({device.id: device})
             _LOG.info('Device loaded', extra={'device_id': dev_id})
-        except (ValidationError, AttributeError,) as e:
+        except (ValidationError,) as e:  #
             _LOG.warning('Cannot load device',
                          extra={'device_id': dev_id, 'exc': e, })
-        except (TypeError, Exception) as e:
+        except (TypeError, AttributeError, Exception) as e:
             _LOG.exception('Unhandled load device exception',
                            extra={'device_id': dev_id, 'exc': e, })
 
