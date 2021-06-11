e8e12cb9c63cf9b7aeaa1c528d56a3b8f0b6ab78
mtovts <lllghtz@outlook.com>
2021-06-12 02:09:12 +0300

    type hotfix add

--- gateway/models/bacnet/base_obj.py
+++ gateway/models/bacnet/base_obj.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, Json
+from pydantic import BaseModel, Field, Json, validator
 
 from .obj_property import ObjProperty
 from .obj_type import ObjType
@@ -21,6 +21,13 @@ class BaseBACnetObjModel(BaseModel):
     characters used in the Object_Name shall be restricted to printable characters.''')
 
     property_list: Json = Field(alias=ObjProperty.propertyList.id_str)
+
+    @validator('type')
+    def temp_fix_to_correct_types(cls, v: ObjType) -> ObjType:
+        # FIXME: delete (hotfix to correct invalid type on server)
+        if v is ObjType.ANALOG_VALUE:
+            v = ObjType.ANALOG_OUTPUT
+        return v
 
     @property
     def replaced_name(self) -> str:
