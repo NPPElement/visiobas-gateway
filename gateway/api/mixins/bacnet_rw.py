5486ed84fcb95cac0f04b99d9424f20e192e058b
mtovts <lllghtz@outlook.com>
2021-06-12 00:20:53 +0300

    Update Bacnet rw mixin

--- gateway/api/mixins/bacnet_rw.py
+++ gateway/api/mixins/bacnet_rw.py
@@ -1,54 +1,64 @@
-# from logging import getLogger
-# from typing import Any
-#
-# from ...connectors import BACnetDevice
-# from ...models import BACnetObj, ObjProperty
-#
-# _log = getLogger(__name__)
-#
-#
-# class BACnetRWMixin:
-#
-#     @staticmethod
-#     def read_bacnet(prop: ObjProperty, obj: BACnetObj, device: BACnetDevice) -> Any:
-#         try:
-#             value = device.read_property(obj=obj, prop=prop)
-#             return value
-#         except Exception as e:
-#             _log.warning('Error: {e}',
-#                          exc_info=True
-#                          )
-#
-#     @staticmethod
-#     def write_bacnet(value, prop: ObjProperty, priority: int,
-#                      obj: BACnetObj, device: BACnetDevice) -> bool:
-#         """
-#         :return: is write successful
-#         """
-#         try:
-#             return device.write_property(value=value,
-#                                          prop=prop,
-#                                          priority=priority,
-#                                          obj=obj
-#                                          )
-#         except Exception as e:
-#             _log.warning(f'Error: {e}',
-#                          exc_info=True
-#                          )
-#
-#     def write_with_check_bacnet(self, value, prop: ObjProperty, priority: int,
-#                                 obj: BACnetObj, device: BACnetDevice) -> bool:
-#         """
-#         :return: the read value is equal to the written value
-#         """
-#         self.write_bacnet(value=value,
-#                           prop=prop,
-#                           priority=priority,
-#                           obj=obj,
-#                           device=device
-#                           )
-#         rvalue = self.read_bacnet(prop=prop,
-#                                   obj=obj,
-#                                   device=device
-#                                   )
-#         return value == rvalue
+from typing import Any
+
+from ...models import BACnetObj, ObjProperty
+from ...utils import get_file_logger
+
+_LOG = get_file_logger(name=__name__)
+
+# Aliases
+BACnetDeviceAlias = Any  # '...devices.BACnetDevice'
+
+
+class BACnetRWMixin:
+
+    @staticmethod
+    async def read_bacnet(prop: ObjProperty, obj: BACnetObj, device: BACnetDeviceAlias
+                          ) -> Any:
+        try:
+            value = await device.async_read_property(obj=obj, prop=prop)
+            return value
+        except Exception as e:
+            _LOG.exception('Unhandled error',
+                           extra={'device_id': device.id, 'object_id': obj.id,
+                                  'object_type': obj.type, 'exc': e, })
+
+    @staticmethod
+    async def write_bacnet(value, prop: ObjProperty, priority: int,
+                           obj: BACnetObj, device: BACnetDeviceAlias) -> bool:
+        """
+        Args:
+            value: Value to read.
+            prop: Property for value.
+            priority: Priority of value.
+            obj: Object instance.
+            device: Device instance.
+
+        Returns:
+            Is write successful.
+        """
+        try:
+            return await device.async_write_property(value=value,
+                                                     prop=prop,
+                                                     priority=priority,
+                                                     obj=obj
+                                                     )
+        except Exception as e:
+            _LOG.exception('Unhandled error',
+                           extra={'device_id': device.id, 'object_id': obj.id, 'exc': e, })
+
+    # def write_with_check_bacnet(self, value, prop: ObjProperty, priority: int,
+    #                             obj: BACnetObj, device: BACnetDeviceAlias) -> bool:
+    #     """
+    #     :return: the read value is equal to the written value
+    #     """
+    #     self.write_bacnet(value=value,
+    #                       prop=prop,
+    #                       priority=priority,
+    #                       obj=obj,
+    #                       device=device
+    #                       )
+    #     rvalue = self.read_bacnet(prop=prop,
+    #                               obj=obj,
+    #                               device=device
+    #                               )
+    #     return value == rvalue
