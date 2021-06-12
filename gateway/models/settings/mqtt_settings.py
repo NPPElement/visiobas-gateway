90e2cf0cd548905e05ac39e8a9285c798204b79c
mtovts <lllghtz@outlook.com>
2021-06-12 16:52:11 +0300

    Add mqtt settings + template

---
+++ gateway/models/settings/mqtt_settings.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings, Field, AnyUrl
+
+
+class MQTTSettings(BaseSettings):
+    url: AnyUrl = Field(...)
+
+    qos: int = Field(default=0, ge=0, le=2)
+    retain: bool = Field(default=True)
+
+    topics_sub: list[str] = Field(default=None)
+
+    class Config:
+        # arbitrary_types_allowed = True
+        env_prefix = 'GTW_MQTT_'
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
