ebc39958d3938516ff068d169b452c8fdc5e35d3
mtovts <lllghtz@outlook.com>
2021-06-13 21:47:16 +0300

    Update git_update.sh

--- run/git_update.sh
+++ run/git_update.sh
@@ -2,6 +2,8 @@ sudo docker-compose down
 # todo cancel changes
 sudo git pull
 
+sudo rm -r dist/  # remove previous source distribution
+
 sudo python3 setup.py sdist  # create a source distribution
 
 sudo docker-compose up --build
