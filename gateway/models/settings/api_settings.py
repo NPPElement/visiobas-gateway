42593324cd058ec3ca6db74198af973ba8d486a6
mtovts <lllghtz@outlook.com>
2021-06-18 22:45:10 +0300

    Add API settings template + model

---
+++ gateway/models/settings/api_settings.py
@@ -0,0 +1,19 @@
+from pydantic import (Field, BaseSettings, AnyHttpUrl)
+
+
+class ApiSettings(BaseSettings):
+    url: AnyHttpUrl = Field(default='http://0.0.0.0:7070', description='Url to run API.')
+    priority: int = Field(default=11, description='Priority to write through gateway API.')
+
+    class Config:
+        env_prefix = 'GTW_API_'
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+    @property
+    def host(self) -> str:
+        return self.url.host
+
+    @property
+    def port(self) -> int:
+        return int(self.url.port)
