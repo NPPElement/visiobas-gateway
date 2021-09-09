2ea54561fa9081a21e99a4600721ffc1486c83ca
Matvey <38750524+mtovts@users.noreply.github.com>
2021-09-09 21:38:14 +0300

    fix logs path

--- visiobas_gateway/models/settings/log_settings.py
+++ visiobas_gateway/models/settings/log_settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pydantic import BaseSettings, Field
+from visiobas_gateway import BASE_DIR
 
 
 class LogSettings(BaseSettings):
@@ -11,10 +12,10 @@ class LogSettings(BaseSettings):
     file_size = Field(default=50, description="Number of MB for log files.")
     format: str = Field(
         default="%(levelname)-8s [%(asctime)s] "
-        "%(name)s.%(funcName)s(%(lineno)d): %(message)s"
+                "%(name)s.%(funcName)s(%(lineno)d): %(message)s"
     )  # [%(threadName)s]
     disable_loggers: list[str] = Field(default_factory=list)
-    log_dir: Path = Field(default=Path(".gateway_logs"))  # todo
+    log_dir: Path = Field(default=BASE_DIR.parent / ".gtw_logs")
 
     # @validator("disable_loggers", pre=True)
     # def spit_names(cls, value: list[str]) -> list[str]:
