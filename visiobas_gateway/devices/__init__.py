f7fef16f9b6656bb52f7999db2039cb6c6fad7c3
Matvey <38750524+mtovts@users.noreply.github.com>
2021-10-10 19:34:02 +0300

    comment unused `SunAPI`

--- visiobas_gateway/devices/__init__.py
+++ visiobas_gateway/devices/__init__.py
@@ -2,12 +2,13 @@ from .bacnet.bacnet import BACnetDevice
 from .base_device import BaseDevice
 from .base_polling_device import BasePollingDevice
 from .modbus.modbus import ModbusDevice
-from .sunapi.sunapi import SUNAPIDevice
+
+# from .sunapi.sunapi import SUNAPIDevice
 
 __all__ = [
     "ModbusDevice",
     "BACnetDevice",
-    "SUNAPIDevice",
+    # "SUNAPIDevice",
     "BaseDevice",
     "BasePollingDevice",
 ]
--- visiobas_gateway/devices/sunapi/sunapi.py
+++ visiobas_gateway/devices/sunapi/sunapi.py
@@ -1,70 +1,70 @@
-from typing import Any
-
-from ...schemas.bacnet.device_property_list import TcpIpDevicePropertyList
-from ..base_device import BaseDevice
-
-
-class SUNAPIDevice(BaseDevice):
-    """Now used for XNP-6550-RH."""
-
-    def ptz(self, cgi: str, submenu: str, action: str, **kwargs: Any) -> None:
-        """The absolute submenu of `ptzcontrol.cgi` controls absolute a PTZ operation
-        that moves the camera to the specified position.
-
-        # todo: add check
-        """
-        assert isinstance(self._device_obj.property_list, TcpIpDevicePropertyList)
-
-        control_submenus = {
-            "absolute",
-            "relative",
-            "continuous",
-            "query",
-            "preset",
-            "swing",
-            "group",
-            "tour",
-            "trace",
-            "home",
-            "areazoom",
-            "stop",
-            "move",
-            "aux",
-            "digitalautotracking",
-            "rs485Command",
-            "osdmenu",
-        }
-        config_submenus = {
-            "swing",
-            "group",
-            "tour",
-            "trace",
-            "autorun",
-            "home",
-            "preset",
-            "presetimageconfig",
-            "presetvideoanalysis",
-            "Presetvideoanalysis2",
-            "ptzsettings",
-        }
-        # actions = {}  # todo
-
-        if cgi == "control":
-            if submenu not in control_submenus:
-                raise ValueError(f"Control submenu must be one of {control_submenus}")
-        elif cgi == "config":
-            if submenu not in config_submenus:
-                raise ValueError(f"Config submenu must be one of {config_submenus}")
-        else:
-            raise ValueError("Provide `control` or `config` cgi")
-
-        cgi = "ptz" + cgi + ".cgi"
-        url_path = f"http://{self._device_obj.property_list.address}/stw‐cgi/{cgi}"
-        # ex = 'http://<Device IP>/stw‐cgi/ptzcontrol.cgi?
-        # msubmenu=absolute&action=control&Pan=90&Zoom=30&Tilt=25'
-        if self._gtw.http_client is not None:
-            self._gtw.http_client.request(
-                method="POST",
-                url=url_path,
-                params={"msubmenu": submenu, "action": action, **kwargs},
-            )
+# from typing import Any
+#
+# from ...schemas.bacnet.device_property_list import TcpIpDevicePropertyList
+# from ..base_device import BaseDevice
+#
+#
+# class SUNAPIDevice(BaseDevice):
+#     """Now used for XNP-6550-RH."""
+#
+#     def ptz(self, cgi: str, submenu: str, action: str, **kwargs: Any) -> None:
+#         """The absolute submenu of `ptzcontrol.cgi` controls absolute a PTZ operation
+#         that moves the camera to the specified position.
+#
+#         # todo: add check
+#         """
+#         assert isinstance(self._device_obj.property_list, TcpIpDevicePropertyList)
+#
+#         control_submenus = {
+#             "absolute",
+#             "relative",
+#             "continuous",
+#             "query",
+#             "preset",
+#             "swing",
+#             "group",
+#             "tour",
+#             "trace",
+#             "home",
+#             "areazoom",
+#             "stop",
+#             "move",
+#             "aux",
+#             "digitalautotracking",
+#             "rs485Command",
+#             "osdmenu",
+#         }
+#         config_submenus = {
+#             "swing",
+#             "group",
+#             "tour",
+#             "trace",
+#             "autorun",
+#             "home",
+#             "preset",
+#             "presetimageconfig",
+#             "presetvideoanalysis",
+#             "Presetvideoanalysis2",
+#             "ptzsettings",
+#         }
+#         # actions = {}  # todo
+#
+#         if cgi == "control":
+#             if submenu not in control_submenus:
+#                 raise ValueError(f"Control submenu must be one of {control_submenus}")
+#         elif cgi == "config":
+#             if submenu not in config_submenus:
+#                 raise ValueError(f"Config submenu must be one of {config_submenus}")
+#         else:
+#             raise ValueError("Provide `control` or `config` cgi")
+#
+#         cgi = "ptz" + cgi + ".cgi"
+#         url_path = f"http://{self._device_obj.property_list.address}/stw‐cgi/{cgi}"
+#         # ex = 'http://<Device IP>/stw‐cgi/ptzcontrol.cgi?
+#         # msubmenu=absolute&action=control&Pan=90&Zoom=30&Tilt=25'
+#         if self._gtw.http_client is not None:
+#             self._gtw.http_client.request(
+#                 method="POST",
+#                 url=url_path,
+#                 params={"msubmenu": submenu, "action": action, **kwargs},
+#             )
