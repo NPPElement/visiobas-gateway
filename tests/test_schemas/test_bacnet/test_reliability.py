64229889ba42302f354bc5214314b41dbcdefbd1
Matvey <38750524+mtovts@users.noreply.github.com>
2021-10-12 20:57:31 +0300

    add test for reliability bacnet shchema

---
+++ tests/test_schemas/test_bacnet/test_reliability.py
@@ -0,0 +1,19 @@
+import pytest
+from visiobas_gateway.schemas.bacnet.reliability import Reliability
+
+
+class TestPriority:
+    @pytest.mark.parametrize(
+        "value",
+        [*{i for i in range(16)} - {11}],
+    )
+    def test_construct_happy(self, value):
+        assert Reliability(value).value == value
+
+    @pytest.mark.parametrize(
+        "value",
+        ["bad_value", 17, -1, None, ""],
+    )
+    def test_bad_value(self, value):
+        with pytest.raises(ValueError):
+            Reliability(value)
