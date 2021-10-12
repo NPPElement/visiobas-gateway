fce7de605402ec5401859d1655fdc4e63caa496a
Matvey <38750524+mtovts@users.noreply.github.com>
2021-10-12 07:19:09 +0300

    add checks to BaseDevice test

--- tests/test_devices/test_base_device.py
+++ tests/test_devices/test_base_device.py
@@ -1,8 +1,18 @@
 from visiobas_gateway.schemas.protocol import Protocol
+from visiobas_gateway.devices.base_device import BaseDevice
+import logging
+from visiobas_gateway.gateway import Gateway
+from visiobas_gateway.schemas.bacnet.device_obj import DeviceObj
 
 
 class TestBaseDevice:
     async def test_construct(self, base_device_factory):
         base_device = base_device_factory()
+
+        assert isinstance(base_device, BaseDevice)
+        assert isinstance(base_device._gtw, Gateway)
+        assert isinstance(base_device._device_obj, DeviceObj)
+
         assert base_device.id == 75
         assert base_device.protocol == Protocol.MODBUS_RTU
+        assert isinstance(base_device._LOG, logging.Logger)
