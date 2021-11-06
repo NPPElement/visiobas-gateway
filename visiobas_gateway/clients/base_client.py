7e9e6a2ae559c89a62da87d02bd70c51a1cb45d6
Matvey <38750524+mtovts@users.noreply.github.com>
2021-11-06 15:17:15 +0300

    complete abc client

--- visiobas_gateway/clients/base_client.py
+++ visiobas_gateway/clients/base_client.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import aiojobs
 from pydantic import BaseSettings
@@ -20,7 +20,8 @@ class AbstractBaseClient(ABC):
     def __init__(self, gateway: Gateway, settings: BaseSettings):
         self._gtw = gateway
         self._settings = settings
-        self._scheduler: aiojobs.Scheduler = None # type: ignore
+        self._client: Any = None
+        self._scheduler: aiojobs.Scheduler = None  # type: ignore
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}({self._settings})'
@@ -29,20 +30,33 @@ class AbstractBaseClient(ABC):
     async def create(cls, gateway: Gateway, settings: BaseSettings) -> AbstractBaseClient:
         """Client factory. Use it to create the client."""
         client = cls(gateway=gateway, settings=settings)
-        client._scheduler = await aiojobs.create_scheduler(close_timeout=60, limit=None)
+        await client.init_client(settings=settings)
+        _LOG.info('Client created', extra={'client': cls.__name__, 'settings': settings})
         return client
 
     @abstractmethod
-    async def _startup_tasks(self):
+    async def init_client(self, settings: BaseSettings) -> None:
+        """Initializes required client."""
+
+    @abstractmethod
+    async def _startup_tasks(self) -> None:
         """ """
 
     @abstractmethod
-    async def _shutdown_tasks(self):
+    async def _shutdown_tasks(self) -> None:
         """ """
 
-    @abstractmethod
     async def start(self) -> None:
         """ """
+        _LOG.debug('Starting client', extra={'client': self.__class__.__name__})
+        self._scheduler = await aiojobs.create_scheduler(close_timeout=5, limit=None)
+        await self._startup_tasks()
+        _LOG.info('Client started', extra={'client': self.__class__.__name__, 'settings':
+            repr(self._settings)})
 
-
-
+    async def stop(self) -> None:
+        """ """
+        _LOG.debug('Stopping client', extra={'client': self.__class__.__name__})
+        await self._shutdown_tasks()
+        await self._scheduler.close()
+        _LOG.info('Client stopped', extra={'client': self.__class__.__name__})
