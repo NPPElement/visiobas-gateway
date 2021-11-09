46da8a1d328c751f34e914ef29e4736313b8078f
Matvey <38750524+mtovts@users.noreply.github.com>
2021-11-09 15:39:56 +0300

    add docs to SendMethod

--- visiobas_gateway/schemas/send_methods.py
+++ visiobas_gateway/schemas/send_methods.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class SendMethod(str, Enum):
+    """Enumeration of all possible ways to send data (clients)."""
+
     HTTP = "http"
     MQTT = "mqtt"
     # SERIAL = 'serial'
