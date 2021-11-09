fe1dfdb310845e03b569bf64c0960b99da015a3b
Matvey <38750524+mtovts@users.noreply.github.com>
2021-11-09 15:37:32 +0300

    Add tests for mqtt, http settings

---
+++ tests/test_schemas/test_settings/test_mqtt_settings.py
@@ -0,0 +1,12 @@
+from visiobas_gateway.schemas.settings import MQTTSettings
+from visiobas_gateway import BASE_DIR
+
+
+class TestMQTTSettings:
+    def test_load_template_env(self):
+        """Tests that template is actual."""
+        from dotenv import load_dotenv
+
+        template_path = BASE_DIR.parent / "config/template.env"
+        load_dotenv(dotenv_path=template_path)
+        isinstance(MQTTSettings(), MQTTSettings)
