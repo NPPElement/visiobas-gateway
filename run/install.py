9b3d1357f8ab4de619e7b6c81a74344209a853c7
Matvey <38750524+mtovts@users.noreply.github.com>
2021-11-23 19:04:46 +0300

    🌪 Feat/install script (#54)
    * add install script
    
    * Update install.py
    
    * Update install.sh
    
    * Update install.sh
    
    * fix urls
    
    * install requests
    
    * capture output in shell calls
    
    * fix links

---
+++ run/install.py
@@ -0,0 +1,153 @@
+from __future__ import print_function
+
+import os
+from pathlib import Path
+import requests
+import pip
+import subprocess
+
+UNAME_TYPE = subprocess.run(['uname', '-m'], capture_output=True).stdout.decode().strip('\n')
+MACHINE_TYPE = subprocess.run(['dpkg', '--print-architecture'], capture_output=True).stdout.decode().strip('\n')
+
+INSTALL_DIRECTORY = Path('/opt/visiobas-gateway')
+INSTALL_DIRECTORY.mkdir(exist_ok=True)
+
+CONFIG_DIRECTORY = (INSTALL_DIRECTORY / 'config')
+CONFIG_DIRECTORY.mkdir(exist_ok=True)
+
+DOCKER_COMPOSE_YAML_PATH = INSTALL_DIRECTORY / 'docker-compose.yaml'
+ENV_CONFIG_PATH = CONFIG_DIRECTORY / '.env'
+
+
+class Installer:
+    DOCKER_CLIENT_DEPENDENCIES = [
+        'apt-transport-https',
+        'ca-certificates',
+        'curl',
+        'gnupg2',
+        'software-properties-common',
+    ]
+
+    @staticmethod
+    def important_msg(msg):
+        print(
+            '''
+            ******************************
+            %s
+            ******************************
+            ''' % msg
+        )
+
+    @staticmethod
+    def run_cmd_with_check(cmd):
+        code = os.system(cmd)
+        if code:
+            raise RuntimeError('Return code: %d for `%s`' % (code, cmd))
+
+    @staticmethod
+    def download_file(url, path):
+        response = requests.get(url)
+        if response.status_code == 200:
+            path.write_bytes(response.content)
+            # with open("DOCKER_COMPOSE_YAML", 'wb') as f:
+            #     f.write()
+        else:
+            raise RuntimeError(
+                'Cannot download `%s` HTTP code: %s' % (
+                    DOCKER_COMPOSE_YAML_PATH, response.status_code
+                )
+            )
+
+    def install_docker_engine(self):
+        """
+        Returns:
+            Is installed successful.
+        Raises:
+            RuntimeError if return code any of cmd is not 0.
+        """
+
+        cmd = ' '.join(('apt-get -y install', *self.DOCKER_CLIENT_DEPENDENCIES))
+        self.run_cmd_with_check(cmd)
+
+        self.run_cmd_with_check(
+            'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -')
+        if UNAME_TYPE == 'x86_64':
+            self.run_cmd_with_check('add-apt-repository -y \
+                   "deb [arch=amd64] https://download.docker.com/linux/debian \
+                   $(lsb_release -cs) \
+                   stable"')
+        elif MACHINE_TYPE == 'armhf':
+            self.run_cmd_with_check('add-apt-repository -y \
+                   "deb [arch=armhf] https://download.docker.com/linux/debian \
+                   $(lsb_release -cs) \
+                   stable"')
+        elif MACHINE_TYPE == 'arm64':
+            self.run_cmd_with_check('add-apt-repository -y \
+                   "deb [arch=arm64] https://download.docker.com/linux/debian \
+                   $(lsb_release -cs) \
+                   stable"')
+        else:
+            raise RuntimeError(
+                'Could not detect architecture: uname=%s machinery=%s' % (
+                    UNAME_TYPE, MACHINE_TYPE)
+            )
+        self.run_cmd_with_check('apt-get update')
+        self.run_cmd_with_check('apt-get -y install docker-ce-cli')
+        self.run_cmd_with_check('apt install -y docker-ce containerd.io')
+
+    @staticmethod
+    def install_docker_compose():
+        pip.main(['install', 'docker-compose'])
+
+
+if __name__ == '__main__':
+    if os.geteuid() != 0:
+        exit("Must be run as root.\n")
+
+    installer = Installer()
+
+    try:
+
+        installer.run_cmd_with_check('apt-get update -y')
+
+        print('\nInstalling Docker engine\n'.upper())
+        installer.install_docker_engine()
+
+        print('\nInstalling docker-compose\n'.upper())
+        installer.install_docker_compose()
+
+        print('\nAll dependencies installed\n')
+
+        print('\nDownloading `%s`\n' % DOCKER_COMPOSE_YAML_PATH)
+        installer.download_file(
+            url="https://github.com/NPPElement/visiobas-gateway/tree/main/docker-compose.yaml",
+            path=DOCKER_COMPOSE_YAML_PATH
+        )
+
+        print('\nPulling docker image\n'.upper())
+        installer.run_cmd_with_check('docker pull mtovts/visiobas-gateway:latest')
+
+        print('\nDownloading `%s`\n' % ENV_CONFIG_PATH)
+        installer.download_file(
+            url="https://raw.githubusercontent.com/NPPElement/visiobas-gateway/main/config/template.env",
+            path=ENV_CONFIG_PATH
+        )
+
+        installer.important_msg(
+            '''
+            INSTALLATION COMPLETE
+            ---------------------
+            Please, configure gateway in `%s`
+            Then run `docker-compose up` from /opt/visiobas-gateway
+            ''' % CONFIG_DIRECTORY
+        )
+    except Exception as exc:
+        # TODO add traceback
+        installer.important_msg(
+            '''
+            INSTALLATION FAILED
+            -------------------
+            Please contact to VisioBAS Gateway developers and provide this message:
+            %s
+            ''' % exc
+        )
--- run/install.sh
+++ run/install.sh
@@ -1,49 +1,67 @@
-# FIXME: Outdated script! Will be updated soon.
+#!/bin/bash
+
+# To install visiobas-gateway run:
+# curl -sSL https://raw.githubusercontent.com/NPPElement/visiobas-gateway/main/run/install.sh | bash -
+
+if [ "$EUID" -ne 0 ]; then
+    printf "Must be run as root.\n"
+    exit 1
+fi
+
+apt-get update -y
+apt-get install -y python3 python3-dev python3-setuptools libssl-dev curl
+
+/usr/lib/python3/dist-packages/easy_install.py pip
+python3 -m pip install --upgrade pip
+python3 -m pip install requests
+
+curl -sSL https://raw.githubusercontent.com/NPPElement/visiobas-gateway/main/run/install.py | python3 -
+
 
 # Updates packages
-sudo apt --assume-yes update
-sudo apt --assume-yes upgrade
+#sudo apt --assume-yes update
+#sudo apt --assume-yes upgrade
 
 # Install required applications
-sudo apt --assume-yes install docker docker.io
-sudo apt --assume-yes install htop mc curl
-sudo apt --assume-yes install git
-sudo apt --assume-yes install python3
-sudo apt --assume-yes install python3-pip
-sudo pip3 install setuptools
+#sudo apt --assume-yes install docker docker.io
+#sudo apt --assume-yes install curl
+#sudo apt --assume-yes install git
+#sudo apt --assume-yes install python3
+#sudo apt --assume-yes install python3-pip
+#sudo pip3 install setuptools
 
 # Install docker-compose from VisioBAS Cloud
-DESTINATION=/usr/bin/docker-compose
-sudo curl -L https://289122.selcdn.ru/Visiodesk-Cloud/containers/docker-compose-Linux-x86_64 -o $DESTINATION
-#VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
-#sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
-#sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
-sudo chmod 755 $DESTINATION
-docker-compose --version
+#DESTINATION=/usr/bin/docker-compose
+#sudo curl -L https://289122.selcdn.ru/Visiodesk-Cloud/containers/docker-compose-Linux-x86_64 -o $DESTINATION
+##VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+##sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+##sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+#sudo chmod 755 $DESTINATION
+#docker-compose --version
 
 # Install Poetry
-sudo curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry
-.py | python3 -
+#sudo curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry
+#.py | python3 -
 
 # Install visiobas_gateway
-cd /opt/visiobas-visiobas_gateway
+#cd /opt/visiobas-gateway
 #sudo python3 setup.py sdist  # create a source distribution
-sudo ~/.poetry/bin/poetry build  # create a source distribution
+#sudo ~/.poetry/bin/poetry build  # create a source distribution
 
 # Export dependencies from Poetry to requirements.txt
-sudo sudo ~/.poetry/bin/poetry export -f requirements.txt --output requirements.txt
+# sudo sudo ~/.poetry/bin/poetry export -f requirements.txt --output requirements.txt
 #  --without-hashes
 
 # Copy configuration templates into /opt/visiobas-visiobas_gateway/config
-sudo cp /opt/visiobas-visiobas_gateway/config/templates/visiobas_gateway.env /opt/visiobas-visiobas_gateway/config/visiobas_gateway.env
-sudo cp /opt/visiobas-visiobas_gateway/config/templates/http.env /opt/visiobas-visiobas_gateway/config/http.env
-sudo cp /opt/visiobas-visiobas_gateway/config/templates/mqtt.env /opt/visiobas-visiobas_gateway/config/mqtt.env
-sudo cp /opt/visiobas-visiobas_gateway/config/templates/api.env /opt/visiobas-visiobas_gateway/config/api.env
+#sudo cp /opt/visiobas-gateway/config/templates/visiobas_gateway.env /opt/visiobas-visiobas_gateway/config/visiobas_gateway.env
+#sudo cp /opt/visiobas-gateway/config/templates/http.env /opt/visiobas-visiobas_gateway/config/http.env
+#sudo cp /opt/visiobas-gateway/config/templates/mqtt.env /opt/visiobas-visiobas_gateway/config/mqtt.env
+#sudo cp /opt/visiobas-gateway/config/templates/api.env /opt/visiobas-visiobas_gateway/config/api.env
 
 # Run container
-sudo docker-compose up --build -d
-
-echo '************************************'
-echo '     VisioBAS Gateway started!'
-echo '************************************'
+#sudo docker-compose up --build -d
+#
+#echo '************************************'
+#echo '     VisioBAS Gateway started!'
+#echo '************************************'
 
