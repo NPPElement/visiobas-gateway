88741aac6a53cd5f48d503c7ce16176236b999c6
Matvey <38750524+mtovts@users.noreply.github.com>
2021-11-23 19:15:57 +0300

    format the code

--- run/install.py
+++ run/install.py
@@ -1,48 +1,56 @@
 from __future__ import print_function
 
 import os
-from pathlib import Path
-import requests
-import pip
 import subprocess
+from pathlib import Path
 
-UNAME_TYPE = subprocess.run(['uname', '-m'], capture_output=True).stdout.decode().strip('\n')
-MACHINE_TYPE = subprocess.run(['dpkg', '--print-architecture'], capture_output=True).stdout.decode().strip('\n')
+import pip
+import requests
 
-INSTALL_DIRECTORY = Path('/opt/visiobas-gateway')
+UNAME_TYPE = (
+    subprocess.run(["uname", "-m"], capture_output=True).stdout.decode().strip("\n")
+)
+MACHINE_TYPE = (
+    subprocess.run(["dpkg", "--print-architecture"], capture_output=True)
+    .stdout.decode()
+    .strip("\n")
+)
+
+INSTALL_DIRECTORY = Path("/opt/visiobas-gateway")
 INSTALL_DIRECTORY.mkdir(exist_ok=True)
 
-CONFIG_DIRECTORY = (INSTALL_DIRECTORY / 'config')
+CONFIG_DIRECTORY = INSTALL_DIRECTORY / "config"
 CONFIG_DIRECTORY.mkdir(exist_ok=True)
 
-DOCKER_COMPOSE_YAML_PATH = INSTALL_DIRECTORY / 'docker-compose.yaml'
-ENV_CONFIG_PATH = CONFIG_DIRECTORY / '.env'
+DOCKER_COMPOSE_YAML_PATH = INSTALL_DIRECTORY / "docker-compose.yaml"
+ENV_CONFIG_PATH = CONFIG_DIRECTORY / ".env"
 
 
 class Installer:
     DOCKER_CLIENT_DEPENDENCIES = [
-        'apt-transport-https',
-        'ca-certificates',
-        'curl',
-        'gnupg2',
-        'software-properties-common',
+        "apt-transport-https",
+        "ca-certificates",
+        "curl",
+        "gnupg2",
+        "software-properties-common",
     ]
 
     @staticmethod
     def important_msg(msg):
         print(
-            '''
+            """
             ******************************
             %s
             ******************************
-            ''' % msg
+            """
+            % msg
         )
 
     @staticmethod
     def run_cmd_with_check(cmd):
         code = os.system(cmd)
         if code:
-            raise RuntimeError('Return code: %d for `%s`' % (code, cmd))
+            raise RuntimeError("Return code: %d for `%s`" % (code, cmd))
 
     @staticmethod
     def download_file(url, path):
@@ -53,9 +61,8 @@ class Installer:
             #     f.write()
         else:
             raise RuntimeError(
-                'Cannot download `%s` HTTP code: %s' % (
-                    DOCKER_COMPOSE_YAML_PATH, response.status_code
-                )
+                "Cannot download `%s` HTTP code: %s"
+                % (DOCKER_COMPOSE_YAML_PATH, response.status_code)
             )
 
     def install_docker_engine(self):
@@ -66,41 +73,48 @@ class Installer:
             RuntimeError if return code any of cmd is not 0.
         """
 
-        cmd = ' '.join(('apt-get -y install', *self.DOCKER_CLIENT_DEPENDENCIES))
+        cmd = " ".join(("apt-get -y install", *self.DOCKER_CLIENT_DEPENDENCIES))
         self.run_cmd_with_check(cmd)
 
         self.run_cmd_with_check(
-            'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -')
-        if UNAME_TYPE == 'x86_64':
-            self.run_cmd_with_check('add-apt-repository -y \
+            "curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -"
+        )
+        if UNAME_TYPE == "x86_64":
+            self.run_cmd_with_check(
+                'add-apt-repository -y \
                    "deb [arch=amd64] https://download.docker.com/linux/debian \
                    $(lsb_release -cs) \
-                   stable"')
-        elif MACHINE_TYPE == 'armhf':
-            self.run_cmd_with_check('add-apt-repository -y \
+                   stable"'
+            )
+        elif MACHINE_TYPE == "armhf":
+            self.run_cmd_with_check(
+                'add-apt-repository -y \
                    "deb [arch=armhf] https://download.docker.com/linux/debian \
                    $(lsb_release -cs) \
-                   stable"')
-        elif MACHINE_TYPE == 'arm64':
-            self.run_cmd_with_check('add-apt-repository -y \
+                   stable"'
+            )
+        elif MACHINE_TYPE == "arm64":
+            self.run_cmd_with_check(
+                'add-apt-repository -y \
                    "deb [arch=arm64] https://download.docker.com/linux/debian \
                    $(lsb_release -cs) \
-                   stable"')
+                   stable"'
+            )
         else:
             raise RuntimeError(
-                'Could not detect architecture: uname=%s machinery=%s' % (
-                    UNAME_TYPE, MACHINE_TYPE)
+                "Could not detect architecture: uname=%s machinery=%s"
+                % (UNAME_TYPE, MACHINE_TYPE)
             )
-        self.run_cmd_with_check('apt-get update')
-        self.run_cmd_with_check('apt-get -y install docker-ce-cli')
-        self.run_cmd_with_check('apt install -y docker-ce containerd.io')
+        self.run_cmd_with_check("apt-get update")
+        self.run_cmd_with_check("apt-get -y install docker-ce-cli")
+        self.run_cmd_with_check("apt install -y docker-ce containerd.io")
 
     @staticmethod
     def install_docker_compose():
-        pip.main(['install', 'docker-compose'])
+        pip.main(["install", "docker-compose"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if os.geteuid() != 0:
         exit("Must be run as root.\n")
 
@@ -108,46 +122,48 @@ if __name__ == '__main__':
 
     try:
 
-        installer.run_cmd_with_check('apt-get update -y')
+        installer.run_cmd_with_check("apt-get update -y")
 
-        print('\nInstalling Docker engine\n'.upper())
+        print("\nInstalling Docker engine\n".upper())
         installer.install_docker_engine()
 
-        print('\nInstalling docker-compose\n'.upper())
+        print("\nInstalling docker-compose\n".upper())
         installer.install_docker_compose()
 
-        print('\nAll dependencies installed\n')
+        print("\nAll dependencies installed\n")
 
-        print('\nDownloading `%s`\n' % DOCKER_COMPOSE_YAML_PATH)
+        print("\nDownloading `%s`\n" % DOCKER_COMPOSE_YAML_PATH)
         installer.download_file(
             url="https://raw.githubusercontent.com/NPPElement/visiobas-gateway/main/docker-compose.yaml",
-            path=DOCKER_COMPOSE_YAML_PATH
+            path=DOCKER_COMPOSE_YAML_PATH,
         )
 
-        print('\nPulling docker image\n')
-        installer.run_cmd_with_check('docker pull mtovts/visiobas-gateway:latest')
+        print("\nPulling docker image\n")
+        installer.run_cmd_with_check("docker pull mtovts/visiobas-gateway:latest")
 
-        print('\nDownloading `%s`\n' % ENV_CONFIG_PATH)
+        print("\nDownloading `%s`\n" % ENV_CONFIG_PATH)
         installer.download_file(
             url="https://raw.githubusercontent.com/NPPElement/visiobas-gateway/main/config/template.env",
-            path=ENV_CONFIG_PATH
+            path=ENV_CONFIG_PATH,
         )
 
         installer.important_msg(
-            '''
+            """
             INSTALLATION COMPLETE
             ---------------------
             Please, configure gateway in `%s`
             Then run `docker-compose up` from /opt/visiobas-gateway
-            ''' % CONFIG_DIRECTORY
+            """
+            % CONFIG_DIRECTORY
         )
     except Exception as exc:
         # TODO add traceback
         installer.important_msg(
-            '''
+            """
             INSTALLATION FAILED
             -------------------
             Please contact to VisioBAS Gateway developers and provide this message:
             %s
-            ''' % exc
+            """
+            % exc
         )
